02138288940684f6fca11ed82fbea44fb6110f02
Robert T. McGibbon <rmcgibbo@gmail.com>
2021-03-20 14:16:35 -0400

    no-python-tests: recognize pytest 'no tests ran' in regex

--- rust-checks/src/bin/no-python-tests.rs
+++ rust-checks/src/bin/no-python-tests.rs
@@ -12,14 +12,34 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn analyze_single_file(log: BufReader<ChildStdout>, attr: &Attr) -> Result<Report, Box<dyn Error>> {
-    let re = Regex::new(r"Ran 0 tests in \d+.\d+s").unwrap();
+    // Looking for two kinds of messages in the log that indicate no tests were run. The
+    // first is from `setuptoolsCheckPhase`, and looks like
+
+    // running build_ext
+    // ----------------------------------------------------------------------
+    // Ran 0 tests in 0.000s
+    // OK
+    // Finished executing setuptoolsCheckPhase
+
+    // The second is from pytest (either invoked manually or from pytestCheckHook)
+    // and looks like
+    // ============================= test session starts ==============================
+    // platform linux -- Python 3.8.8, pytest-6.2.2, py-1.9.0, pluggy-0.13.1
+    // rootdir: /build/humanize-3.1.0, configfile: tox.ini
+    // collected 0 items
+    // ============================ no tests ran in 0.00s =============================
+    let re = Regex::new(r"(Ran 0 tests|no tests ran\x1b\[0m\x1b\[33m) in \d+.\d+s").unwrap();
+    let ansi_escape = Regex::new(r"\x1b\[0m\x1b\[33m").unwrap();
 
     let report = log
         .lines()
         .filter_map(|line| line.ok())
         .filter_map(|s| re.find(&s).and_then(|m| Some(m.as_str().to_string())))
         .map(|m| NixpkgsHammerMessage {
-            msg: format!("Test runner could not discover any test cases: ‘{}’", m),
+            msg: format!(
+                "Test runner could not discover any test cases: ‘{}’",
+                ansi_escape.replace(&m, "")
+            ),
             name: "no-python-tests",
             locations: attr.location.iter().map(|x| x.clone()).collect(),
             link: true,
