ab1354ac41fc0c9e0d719d02c9db8ffec8f66737
Robert T. McGibbon <rmcgibbo@gmail.com>
2021-04-03 12:39:23 -0400

    unnecessary-parallel-building: make more precise

--- overlays/unnecessary-parallel-building.nix
+++ overlays/unnecessary-parallel-building.nix
@@ -9,8 +9,14 @@ let
       cond =
         let
           inputs = drvArgs.nativeBuildInputs or [ ] ++ drvArgs.propagatedBuildInputs or [ ] ++ drvArgs.nativeBuildInputs or [ ];
+          toolConfigureHookNotDisabled =
+            (lib.elem prev.meson inputs && !drvArgs.dontUseMesonConfigure or false) ||
+            (lib.elem prev.cmake inputs && !drvArgs.dontUseCmakeConfigure or false) ||
+            (lib.elem prev.qt5.qmake inputs && !drvArgs.dontUseQmakeConfigure or false);
+          hasEnableParallelBuilding = drvArgs.enableParallelBuilding or false;
+          hasDefaultConfigurePhase = ! (drvArgs ? configurePhase);
         in
-          (lib.elem prev.meson inputs || lib.elem prev.cmake inputs || lib.elem prev.qt5.qmake inputs) && drvArgs.enableParallelBuilding or false;
+          toolConfigureHookNotDisabled && hasEnableParallelBuilding && hasDefaultConfigurePhase;
       msg = ''
         Meson, CMake and qmake already set `enableParallelBuilding = true` by default so it is not necessary.
       '';
