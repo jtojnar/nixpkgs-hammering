dae7f70acb57d2593fc244a17f28fc272ac3bf5b
Jan Tojnar <jtojnar@gmail.com>
2021-04-29 01:04:53 +0200

    Prepare tests for nixpkgs update
    Since https://github.com/NixOS/nixpkgs/pull/118283, fetchurl’s and therefore also fetchpatch’s “url” argument needs to be a string. We also need to discard the string context otherwise Nix will complain.
    
    This fixes the evaluation but the resulting derivation will not build (just like it did not before), since scheme-less URIs nor file protocol are supported by curl.

--- tests/missing-patch-comment/ignore-nested-lists2.nix
+++ tests/missing-patch-comment/ignore-nested-lists2.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
   patches = lib.optionals (true) [
     # comment here. no comment is required next to "foo" and "bar"
     (fetchpatch {
-        url = ../fixtures/patch.patch;
+        url = builtins.unsafeDiscardStringContext "file://${../fixtures/patch.patch}";
         sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
         excludes = ["foo" "bar"];
     })
