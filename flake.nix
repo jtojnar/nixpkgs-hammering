fa0acf3c4e055e90fde1031cf9e5054effbd06e5
Jan Tojnar <jtojnar@gmail.com>
2022-05-12 01:33:58 +0200

    flake: update
    Update for flake scheme changes and bump dependencies:
    
    • Updated input 'flake-compat':
        'github:edolstra/flake-compat/b7547d3eed6f32d06102ead8991ec52ab0a4f1a7' (2022-01-03)
      → 'github:edolstra/flake-compat/b4a34015c698c7793d592d66adbab377907a2be8' (2022-04-19)
    • Updated input 'naersk':
        'github:nmattia/naersk/2fc8ce9d3c025d59fee349c1f80be9785049d653' (2021-12-19)
      → 'github:nmattia/naersk/f21309b38e1da0d61b881b6b6d41b81c1aed4e1d' (2022-05-03)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/3eb07eeafb52bcbf02ce800f032f18d666a9498d' (2022-03-15)
      → 'github:NixOS/nixpkgs/d2fc6856824cb87742177eefc8dd534bdb6c3439' (2022-05-11)
    • Updated input 'utils':
        'github:numtide/flake-utils/3cecb5b042f7f209c56ffd8371b2711a290ec797' (2022-02-07)
      → 'github:numtide/flake-utils/a4b154ebbdc88c8498a5c7b01589addc9e9cb678' (2022-04-11)

--- flake.nix
+++ flake.nix
@@ -18,65 +18,80 @@
   };
 
   outputs = { self, flake-compat, naersk, nixpkgs, utils }: {
-    overlay = final: prev: {
-      nixpkgs-hammering =
-        let
-          naersk-lib = prev.callPackage naersk {};
+    overlays = {
+      default =
+        final:
+        prev:
 
-          rust-checks = naersk-lib.buildPackage {
-            name = "rust-checks";
-            root = ./rust-checks;
-          };
+        {
+          nixpkgs-hammering =
+            let
+              naersk-lib = prev.callPackage naersk {};
 
-          # Find all of the binaries installed by rust-checks. Note, if this changes
-          # in the future to use wrappers or something else that pollute the bin/
-          # directory, this logic will have to grow.
-          rust-check-names = let
-            binContents = builtins.readDir "${rust-checks}/bin";
-          in
-            prev.lib.mapAttrsToList (name: type: assert type == "regular"; name) binContents;
-        in
-          prev.runCommand "nixpkgs-hammering" {
-            buildInputs = with prev; [
-              python3
-              makeWrapper
-            ];
+              rust-checks = naersk-lib.buildPackage {
+                name = "rust-checks";
+                root = ./rust-checks;
+              };
 
-            passthru = {
-              inherit rust-checks;
-              exePath = "/bin/nixpkgs-hammer";
-            };
-          } ''
-            install -D ${./tools/nixpkgs-hammer} $out/bin/nixpkgs-hammer
-            patchShebangs $out/bin/nixpkgs-hammer
+              # Find all of the binaries installed by rust-checks. Note, if this changes
+              # in the future to use wrappers or something else that pollute the bin/
+              # directory, this logic will have to grow.
+              rust-check-names = let
+                binContents = builtins.readDir "${rust-checks}/bin";
+              in
+                prev.lib.mapAttrsToList (name: type: assert type == "regular"; name) binContents;
+            in
+              prev.runCommand "nixpkgs-hammering" {
+                buildInputs = with prev; [
+                  python3
+                  makeWrapper
+                ];
 
-            wrapProgram "$out/bin/nixpkgs-hammer" \
-                --prefix PATH ":" ${prev.lib.makeBinPath [
-                  prev.nixVersions.stable or prev.nixUnstable
-                  rust-checks
-                ]} \
-                --set AST_CHECK_NAMES ${prev.lib.concatStringsSep ":" rust-check-names}
-            ln -s ${./overlays} $out/overlays
-            ln -s ${./lib} $out/lib
-          '';
+                passthru = {
+                  inherit rust-checks;
+                  exePath = "/bin/nixpkgs-hammer";
+                };
+              } ''
+                install -D ${./tools/nixpkgs-hammer} $out/bin/nixpkgs-hammer
+                patchShebangs $out/bin/nixpkgs-hammer
+
+                wrapProgram "$out/bin/nixpkgs-hammer" \
+                    --prefix PATH ":" ${prev.lib.makeBinPath [
+                      prev.nixVersions.stable or prev.nixUnstable
+                      rust-checks
+                    ]} \
+                    --set AST_CHECK_NAMES ${prev.lib.concatStringsSep ":" rust-check-names}
+                ln -s ${./overlays} $out/overlays
+                ln -s ${./lib} $out/lib
+              '';
+        };
     };
   } // utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs { inherit system; };
   in rec {
-    packages.nixpkgs-hammering = (self.overlay pkgs pkgs).nixpkgs-hammering;
+    packages = rec {
+      default = nixpkgs-hammering;
 
-    defaultPackage = self.packages.${system}.nixpkgs-hammering;
+      nixpkgs-hammering = (self.overlays.default pkgs pkgs).nixpkgs-hammering;
+    };
 
-    apps.nixpkgs-hammer = utils.lib.mkApp { drv = self.packages.${system}.nixpkgs-hammering; };
+    apps = rec {
+      nixpkgs-hammer = utils.lib.mkApp {
+        drv = self.packages.${system}.nixpkgs-hammering;
+      };
 
-    defaultApp = self.apps.${system}.nixpkgs-hammer;
+      default = nixpkgs-hammer;
+    };
 
-    devShell = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        python3
-        rustc
-        cargo
-      ];
+    devShells = {
+      default =
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+            rustc
+            cargo
+          ];
+        };
     };
   });
 }
